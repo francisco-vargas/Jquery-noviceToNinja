19a073a891a3ef454f1e7e36168531e9b8755223
Francisco Vargas <francisco.vargas@moxieusa.com>
2022-12-05 22:34:24 -0600

    v3

--- chapter_06/04_live_event_handler/script.js
+++ chapter_06/04_live_event_handler/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $('#celebs ul a').click(function(e) {
     var url = $(this).attr('href') + '#description';
     $('#biography').html('loading...').load(url);
-    // e.preventDefault();
+    e.preventDefault();
   });
   $('#biography').on('mouseover', function() {
     $(this).css('background-color', 'yellow');
